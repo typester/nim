d9c77909d8d7e7725ec026fe2bea543450770053
Daisuke Murase <typester@cpan.org>
2009-01-14 10:31:57 +0900

    initial commit

---
+++ lib/Nim.pm
@@ -0,0 +1,40 @@
+package Nim;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Nim - Module abstract (<= 44 characters) goes here
+
+=head1 SYNOPSIS
+
+  use Nim;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for this module was created by ExtUtils::ModuleMaker.
+It looks like the author of the extension was negligent enough
+to leave the stub unedited.
+
+Blah blah blah.
+
+=head1 AUTHOR
+
+Daisuke Murase <typester@cpan.org>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (c) 2009 KAYAC Inc. All rights reserved.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
+1;
