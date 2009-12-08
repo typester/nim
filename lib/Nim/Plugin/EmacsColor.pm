e150170a08f4c02142cbf84ab1769e175f0ac428
Daisuke Murase <typester@cpan.org>
2009-12-08 17:25:13 +0900

    add EmacsColor plugin

---
+++ lib/Nim/Plugin/EmacsColor.pm
@@ -0,0 +1,78 @@
+package Nim::Plugin::EmacsColor;
+use Any::Moose;
+
+with 'Nim::Plugin';
+
+use Text::EmacsColor;
+use HTML::TreeBuilder;
+use Scalar::Util 'blessed';
+
+has emacs_color => (
+    is         => 'rw',
+    isa        => 'Text::EmacsColor',
+    lazy_build => 1,
+);
+
+has filter => (
+    is  => 'rw',
+    isa => 'Str',
+);
+
+has mode => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => 'cperl',
+);
+
+has emacs_command => (
+    is  => 'rw',
+    isa => 'Str',
+);
+
+no Any::Moose;
+
+sub register {
+    my ($self, $context) = @_;
+
+    $context->register_hook(
+        $self,
+        'after_entry.interpolate' => \&process,
+    );
+}
+
+sub process {
+    my ($self, $context, $entry) = @_;
+
+    my $t = HTML::TreeBuilder->new;
+    $t->parse_content($entry->body);
+
+    for my $code ($t->find('code')) {
+        my $html = $code->content->[0];
+
+        if (my $f = $self->filter) {
+            next unless $html =~ /$f/;
+        }
+
+        my $res = $self->emacs_color->format($html, $self->mode);
+        my ($body) = $res->full_html =~ m!<pre>\r?\n(.*)</pre>!s;
+
+        $code->delete_content;
+        $code->push_content(HTML::Element->new('~literal', text => $body));
+    }
+
+    my $result = join '',
+        map { blessed $_ ? $_->as_XML : $_ } $t->find('body')->content_list;
+
+    $t->delete;
+
+    $entry->body($result);
+}
+
+sub _build_emacs_color {
+    my ($self) = @_;
+    Text::EmacsColor->new(
+        $self->emacs_command ? (emacs_command => $self->emacs_command) : (),
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
